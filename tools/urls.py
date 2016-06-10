2b82e00ca0ad290fa4c7758e9de56ea0c8e5d09d
anilreddy123 <anilreddy449@gmail.com>
2016-06-10 17:57:39 +0530

    updated urls

--- tools/urls.py
+++ tools/urls.py
@@ -2,12 +2,12 @@ from django.conf.urls import patterns, include, url
 from .views import *
 
 urlpatterns = patterns('',
-    url(r'^$', 'django.contrib.auth.views.login'),
+    #url(r'^$', 'django.contrib.auth.views.login'),
     url(r'^logout/$', logout_page),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'), # If user is not login it will redirect to login page
     url(r'^register/$', register),
 
-    url(r'^home/$', home),
+    url(r'^$', home, name="home"),
     url(r'^register/success/$', register_success),
     #url(r'^createteam/$', create_team),
     url(r'^addproject/$', addproject),
